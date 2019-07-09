f6048a029de47b7417d2dd4423743cfa00cb9f66
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-07-09 10:58:48 +0300

    Add files via upload

--- vcf_to_ms.py
+++ vcf_to_ms.py
@@ -30,21 +30,28 @@ def transform_genotypes(genotypelist):
     new_genotypelist=[]
     for geno in genotypelist:
         zygocity=geno.count('0')
-        if zygocity==0:
+        missingness=geno.count('.')
+        if zygocity==0 and missingness==0:
             new_genotypelist.append(1)
             new_genotypelist.append(1)
-        if zygocity==1:
+        if zygocity==1 and missingness==0:
             new_genotypelist.append(0)
             new_genotypelist.append(1)
-        if zygocity==2:
+        if zygocity==2 and missingness==0:
+            new_genotypelist.append(0)
+            new_genotypelist.append(0)
+        if missingness!=0: #REQUIRES CORRECTION
             new_genotypelist.append(0)
             new_genotypelist.append(0)
     return new_genotypelist
 
-
+#gather data,split to chunks ,transform them
 begin=0
 counter=0
-CHUNK=[]
+CHUNK=[] #genotypes of each chun go here
+ALL_CHUNKS=[] #each transformed chunk goes in here
+POSITIONS=[] #positions for each chunk go in here
+ALL_POSITIONS=[] #all positions go in here
 for line in VCF_file:
     if line[0]!='#' and line[0]!='!' and line[0]!='\\':
     #only lines with genotypes
@@ -58,12 +65,19 @@ for line in VCF_file:
         CHUNK.append(newgenotypes)
         counter+=1
         end=float(line[1])
-        
-        if end-begin>=1000000:
-        #Chunk is larger than 100000 base length ,write out to ms style
+        if end-begin>=1000000 or begin>=end:
+        #Chunk is larger than 100000 base length or we switch chromosome,write out to ms style
             CHUNK=np.asarray(CHUNK)
-            print(CHUNK.shape)
+            CHUNK=np.transpose(CHUNK)
+            ALL_CHUNKS.append(CHUNK)
             CHUNK=[]
             begin=end
         if counter>=10000:
             break
+#Write to ms file
+            
+for CHUNK in ALL_CHUNKS:
+    for atomo in CHUNK:
+        atomo=np.array2string(atomo)[1:-1]
+        MS_FILE.write(atomo+'\n')
+     MS_FILE.write()
